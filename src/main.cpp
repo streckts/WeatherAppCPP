f2ccd39f2178d3302b3400925a189f84e8aaf233
Streck Salmon <streck0101@gmail.com>
2024-03-17 21:15:57 -0600

    Initialized weather header, weater.cpp, basic input, and added json.hpp for request processing

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main{
+
+    // Take input city, call weather.cpp functions and output results
+    string city_name;
+
+    cout << "Enter a city: ";
+    std::getline(std::cin, city_name);
+
     return 0;
 }
---
+++ src/weather.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../lib/json.hpp"
+#include <curl/curl.h>
+
+using namespace std;
+
+int main() {
+    return 0;
+};
